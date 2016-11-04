cd7dd6d5c2d7b2a563569bbb2369309db184c0fa
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-11-04 16:42:26 +0900

    Add: Add a react-bootstrap and grid

--- front/src/main.js
+++ front/src/main.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ThreeContainer from './components/three-container'
+import { Grid, Row, Col } from 'react-bootstrap'
 
 export default class main extends React.Component {
   constructor(props) {
@@ -9,7 +10,16 @@ export default class main extends React.Component {
   render() {
     return (
       <div>
-        <ThreeContainer />
+        <Grid>
+          <Row className="show-grid">
+            <Col md={8}>
+              <ThreeContainer />
+            </Col>
+            <Col md={4}>
+              aaa
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
