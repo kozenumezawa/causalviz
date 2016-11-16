aa6926189a0dc16061a7db181273f3644e07d726
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-11-16 12:48:25 -0700

    Add: to use TIFF file

--- front/src/components/tiff-container.js
+++ front/src/components/tiff-container.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import TiffViewer from '../tiff-viewer'
 
 export default class TiffContainer extends React.Component{
   constructor(props) {
     super(props);
   }
 
+  componentDidMount() {
+    let test = new TiffViewer();
+  }
+
   render() {
     return (
       <div id="output_space">
-        a
       </div>
     );
   }
