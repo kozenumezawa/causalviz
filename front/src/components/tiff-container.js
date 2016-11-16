93176095a96aff4901858208db3657284195249b
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-11-16 15:44:12 -0700

    Update: [WIP] to use TIFF file using React

--- front/src/components/tiff-container.js
+++ front/src/components/tiff-container.js
@@ -4,15 +4,33 @@ import TiffViewer from '../tiff-viewer'
 export default class TiffContainer extends React.Component{
   constructor(props) {
     super(props);
+    this.tiff_viewer = new TiffViewer();
   }
 
   componentDidMount() {
-    let test = new TiffViewer();
+  }
+
+  getRenderTiffList() {
+    let key = 0;
+    const tiff_list = this.tiff_viewer.getTiffList();
+    const renderList = tiff_list.map((tiffFile) => {
+      return (
+        <div key={key++}>{tiffFile}</div>
+      );
+    });
+    return (
+      <div>
+        {renderList}
+      </div>
+    );
   }
 
   render() {
     return (
       <div id="output_space">
+        {(() => {
+          this.getRenderTiffList()
+        })()}
       </div>
     );
   }
--- front/src/tiff-viewer.js
+++ front/src/tiff-viewer.js
@@ -1,15 +1,21 @@
 export default class TiffViewer {
   constructor () {
+    this.tiff_list = [];
     window.fetch('Substack.tif')
       .then((response) => {
         response.arrayBuffer().then((buffer) => {
           const tiff = new Tiff({ buffer: buffer });
           for (let i = 0, len = tiff.countDirectory(); i < len; i++) {
             tiff.setDirectory(i);
-            var canvas = tiff.toCanvas();
+            const canvas = tiff.toCanvas();
+            this.tiff_list.push(canvas);
             document.body.append(canvas);
           }
         });
       });
+  }
+
+  getTiffList() {
+    return this.tiff_list;
   }
 }
