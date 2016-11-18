9159616b867dbe7659438f68a0f269ca8df3e99d
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-11-18 11:31:11 -0800

    Refactor:

--- front/src/tiff-viewer.js
+++ front/src/tiff-viewer.js
@@ -10,16 +10,15 @@ export default class TiffViewer {
             const canvas = tiff.toCanvas();
             this.tiff_list.push(canvas);
 
-            const new_canvas = document.createElement('canvas');
-            new_canvas.width = canvas.width * 2;
-            new_canvas.height = canvas.height * 2;
-            var ctx = new_canvas.getContext('2d');
-            ctx.drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, canvas.width * 2, canvas.height * 2);
-            // var ctx = canvas.getContext('2d');
-            // ctx.drawImage(canvas, 0, 0, cavwidth, canvas.height, 0, 0, canvas.width * 2, canvas.height * 2);
+            const display_canvas = document.createElement('canvas');
+            const bias = 3;
+            display_canvas.width = canvas.width * bias;
+            display_canvas.height = canvas.height * bias;
+            const ctx = display_canvas.getContext('2d');
+            ctx.drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, canvas.width * bias, canvas.height * bias);
 
             const elem = document.getElementById("output_space");
-            elem.appendChild(new_canvas);
+            elem.appendChild(display_canvas);
           }
         });
       });
