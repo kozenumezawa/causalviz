d5035771c4051ffa772048adbfc3d5507ef8b626
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-11-20 03:43:02 +0900

    Add: realize Flux data flow

--- front/src/components/main.js
+++ front/src/components/main.js
@@ -2,18 +2,31 @@ import React from 'react'
 import { Grid, Row, Col } from 'react-bootstrap'
 
 import Store from '../stores/Store'
+import eventConstants from '../constants/event-constants'
 
 import TiffContainer from './tiff-container'
 import AppBar from './app-bar'
 import StepButton from './button/step-button'
 
+function getAllState() {
+  return {
+    tiff_list: Store.getTiffList()
+  }
+}
 
 export default class main extends React.Component {
   constructor(props) {
     super(props);
+    this.state = getAllState();
+    console.log(getAllState());
   }
 
   componentDidMount() {
+    Store.addChangeListener(this._onChange.bind(this));
+  }
+
+  _onChange() {
+    this.setState(getAllState());
   }
 
   render() {
--- front/src/stores/Store.js
+++ front/src/stores/Store.js
@@ -2,11 +2,53 @@ import { EventEmitter } from 'events'
 import Dispatcher from '../dispatcher/Dispatcher'
 import eventConstants from '../constants/event-constants'
 
+const CHANGE_EVENT = 'change';
+
+let tiff_list = [];
+
 class Store extends EventEmitter {
   constructor() {
     super();
+    this.getTiffData();
+
     Dispatcher.register(this.handler.bind(this));
-    
+  }
+
+  emitChange() {
+    this.emit(CHANGE_EVENT);
+  }
+
+  addChangeListener(callback) {
+    this.on(CHANGE_EVENT, callback);
+  }
+
+  getTiffData() {
+    tiff_list = [];
+    window.fetch('Substack.tif')
+      .then((response) => {
+        response.arrayBuffer().then((buffer) => {
+          const tiff = new Tiff({ buffer: buffer });
+          for (let i = 0, len = tiff.countDirectory(); i < len; i++) {
+            tiff.setDirectory(i);
+            const canvas = tiff.toCanvas();
+            tiff_list.push(canvas);
+            this.appendCanvas(canvas, "output_space");
+          }
+          this.emitChange();
+        });
+      });
+  }
+
+  appendCanvas(canvas, id) {
+    const display_canvas = document.createElement('canvas');
+    const bias = 3;
+    display_canvas.width = canvas.width * bias;
+    display_canvas.height = canvas.height * bias;
+    const ctx = display_canvas.getContext('2d');
+    ctx.drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, canvas.width * bias, canvas.height * bias);
+
+    const elem = document.getElementById(id);
+    elem.appendChild(display_canvas);
   }
 
   handler(action) {
@@ -18,6 +60,10 @@ class Store extends EventEmitter {
         break;
       default:
     }
+  }
+
+  getTiffList() {
+    return tiff_list;
   }
 }
 
