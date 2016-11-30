af74c64868e931426bbb192ee38229bcdfaeb5a7
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-11-30 16:17:14 +0900

    Fix: revise an initial position

--- front/src/components/graph-container.js
+++ front/src/components/graph-container.js
@@ -17,27 +17,26 @@ export default class graphContainer extends React.Component {
 
   lineGraph(canvas_obj, time_series_data, line_opts) {
     const context = canvas_obj.getContext('2d');
-    let pos1 = {
-      x: 0,
-      y: canvas_obj.height
-    };
-    let pos2 = {
-      x: 0,
-      y: 0
-    };
-    const dataWH = {
-      w: canvas_obj.width / time_series_data.length,
-      h: canvas_obj.height
-    };
+
     context.strokeStyle = line_opts.color;
     context.lineWidth = line_opts.width;
     context.save();
     context.beginPath();
+    const dataWH = {
+      w: canvas_obj.width / time_series_data.length,
+      h: canvas_obj.height
+    };
+
     // draw lines
-    for (let i = 0; i < time_series_data.length; i++){
+    let pos1 = {
+      x: dataWH.w,
+      y: dataWH.h - time_series_data[0] / 1.3    // 1.3 is temporal number
+    };
+    let pos2 = pos1;
+    for (let i = 1; i < time_series_data.length; i++){
       context.moveTo(pos1.x, pos1.y);
       pos2.x += dataWH.w;
-      pos2.y = dataWH.h - time_series_data[i];
+      pos2.y = dataWH.h - time_series_data[i] / 1.3;    // 1.3 is temporal number
       context.lineTo(pos2.x, pos2.y);
       pos1 = pos2;
     }
