ab7135ba8bb3e9f11cce0b374a16cc637089ab42
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-12-03 18:31:51 +0900

    Refactor:

--- front/src/components/tiff-container.js
+++ front/src/components/tiff-container.js
@@ -23,13 +23,14 @@ export default class TiffContainer extends React.Component{
       this.display_canvas = document.getElementById(this.props.id);
       this.ctx = this.display_canvas.getContext('2d');
       this.ctx.drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, this.display_canvas.width, this.display_canvas.height);
+
+      const overlapped_canvas = document.getElementById(this.props.id + 'overlapped');
+      overlapped_canvas.addEventListener('click', this.onClickCanvas, false);
     }
     //  Update canvas
     this.ctx.beginPath();
     this.ctx.drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, this.display_canvas.width, this.display_canvas.height);
     this.ctx.closePath();
-
-    this.display_canvas.addEventListener('click', this.onClickCanvas, false);
   }
 
   onClickCanvas(e) {
@@ -37,12 +38,18 @@ export default class TiffContainer extends React.Component{
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
     Action.handleTiffClick(x, y);
+
+    // draw a point
+    const overlapped_canvas = document.getElementById(this.props.id + 'overlapped');
+    const ctx = overlapped_canvas.getContext('2d');
+    ctx.clearRect(0, 0, overlapped_canvas.width, overlapped_canvas.height);
   }
 
   render() {
     return (
       <div>
-        <canvas id={this.props.id} width="280" height="200" ></canvas>
+        <canvas id={this.props.id} width="280" height="200" style={{left: 0, top: 0, zIndex: 0}}></canvas>
+        <canvas id={this.props.id+'overlapped'} width="280" height="200" style={{position: 'absolute', display: 'block', top: 0, zIndex: 1}}></canvas>
         {(() => {
           this.renderTiff();
         })()}
