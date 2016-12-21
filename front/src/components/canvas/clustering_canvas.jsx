1d8f5452bad86100a61ccb96f78592fe4952d6bd
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-12-21 16:49:54 +0900

    Add: add a canvas to draw a clustering result

---
+++ front/src/components/canvas/clustering_canvas.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+
+export default class ClusteringCanvas extends React.Component{
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.canvas = document.getElementById(this.props.id);
+    this.ctx = this.canvas.getContext('2d');
+    this.drawFrame();
+  }
+
+  componentWillReceiveProps(nextProps) {
+  }
+
+  /**
+   * Converts an HSL color value to RGB. Conversion formula
+   * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+   * Assumes h, s, and l are contained in the set [0, 1] and
+   * returns r, g, and b in the set [0, 255].
+   *
+   * @param   {number}  h       The hue
+   * @param   {number}  s       The saturation
+   * @param   {number}  l       The lightness
+   * @return  {Array}           The RGB representation
+   */
+  hslToRgb(h, s, l) {
+    let r, g, b;
+
+    if(s == 0){
+      r = g = b = l; // achromatic
+    }else{
+      const hue2rgb = function hue2rgb(p, q, t){
+        if(t < 0) t += 1;
+        if(t > 1) t -= 1;
+        if(t < 1/6) return p + (q - p) * 6 * t;
+        if(t < 1/2) return q;
+        if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+        return p;
+      };
+
+      var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+      var p = 2 * l - q;
+      r = hue2rgb(p, q, h + 1/3);
+      g = hue2rgb(p, q, h);
+      b = hue2rgb(p, q, h - 1/3);
+    }
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+  }
+
+  // convert [-1, 1] -> [0, 1]
+  colorScale(x) {
+    return 0.5 + (x / 2);
+  }
+
+  drawFrame() {
+    // draw a frame
+    this.ctx.strokeStyle = 'black';
+    this.ctx.lineWidth = 1;
+    this.ctx.beginPath();
+    this.ctx.moveTo(0, 0);
+    this.ctx.lineTo(this.canvas.width, 0);
+    this.ctx.lineTo(this.canvas.width, this.canvas.height);
+    this.ctx.lineTo(0, this.canvas.height);
+    this.ctx.lineTo(0, 0);
+    this.ctx.stroke();
+  }
+
+  render() {
+    return (
+      <div>
+        <canvas id={this.props.id} width="285" height="130" style={{left: 0, top: 0, zIndex: 0}}></canvas>
+      </div>
+    );
+  }
+}
