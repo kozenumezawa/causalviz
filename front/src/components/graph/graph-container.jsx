9c49344e0bbd19a1e2c24ff1e9481fbb409f5fa6
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-12-22 22:19:26 +0900

    Refactor: add a new component for future improvements

--- front/src/components/graph/graph-container.jsx
+++ front/src/components/graph/graph-container.jsx
@@ -1,28 +1,11 @@
 import React from 'react'
 
-import Action from '../../actions/Actions'
+import IndicatorCanvas from './indicator-canvas.jsx'
 
 export default class graphContainer extends React.Component {
   constructor(props) {
     super(props);
-    this.mouse_down = false;
     this.already_drawn = false;
-
-    this.mouseDown = this.mouseDown.bind(this);
-    this.mouseMove = this.mouseMove.bind(this);
-    this.mouseUp = this.mouseUp.bind(this);
-    this.mouseOut = this.mouseOut.bind(this);
-  }
-
-  componentDidMount() {
-    // draw a vertical line to the graph
-    const target_canvas = document.getElementById(this.props.id + '_indicator');
-    this.drawVerticalLine(0, 0, 0, target_canvas.height);
-
-    target_canvas.addEventListener('mousedown', this.mouseDown)
-    target_canvas.addEventListener('mousemove', this.mouseMove)
-    target_canvas.addEventListener('mouseup', this.mouseUp)
-    target_canvas.addEventListener('mouseout', this.mouseOut);
   }
 
   componentDidUpdate(prevProps) {
@@ -37,16 +20,6 @@ export default class graphContainer extends React.Component {
       };
       ctx.clearRect(0, 0, target_canvas.width, target_canvas.height);
       this.lineGraph(target_canvas, time_series[this.props.highlighted_line], line_opts);
-    }
-
-    // draw an indicator to show a timestep
-    if(prevProps.tiff_index !== this.props.tiff_index) {
-      const target_canvas = document.getElementById(this.props.id + '_indicator');
-      const pos1 = {
-        x: target_canvas.width / this.props.tiff_list.length * this.props.tiff_index,
-        y: 0
-      };
-      this.drawVerticalLine(pos1.x, pos1.y, pos1.x, target_canvas.height);
     }
   }
 
@@ -77,54 +50,6 @@ export default class graphContainer extends React.Component {
     context.stroke();
   }
 
-  drawVerticalLine(x1, y1, x2, y2) {
-    const target_canvas = document.getElementById(this.props.id + '_indicator');
-    const ctx = target_canvas.getContext('2d');
-    ctx.strokeStyle = 'black';
-    ctx.lineWidth = 2;
-    ctx.clearRect(0, 0, target_canvas.width, target_canvas.height);
-    // draw lines
-    ctx.beginPath()
-    ctx.moveTo(x1, y1);
-    ctx.lineTo(x2, y2);
-    ctx.stroke();
-  }
-
-  mouseDown(e) {
-    this.updateIndexByMouse(e);
-    this.mouse_down = true;
-  }
-
-  mouseMove(e) {
-    if(this.mouse_down === false) {
-      return;
-    }
-    this.updateIndexByMouse(e);
-  }
-
-  updateIndexByMouse(e) {
-    const rect = e.target.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-
-    const target_canvas = document.getElementById(this.props.id + '_indicator');
-    const line_x = target_canvas.width / this.props.tiff_list.length * this.props.tiff_index;
-
-    let new_index = Math.floor(x / target_canvas.width * this.props.tiff_list.length);
-    new_index = (new_index > 0)? new_index : 0;
-    new_index = (new_index < this.props.tiff_list.length)? new_index : this.props.tiff_list.length;
-    Action.handleIndicatorMove(new_index);
-  }
-
-  mouseUp(e) {
-    this.mouse_down = false;
-  }
-
-  mouseOut(e) {
-    if(this.mouse_down === true) {
-      this.mouse_down = false;
-    }
-  }
-
   renderData() {
     const time_series = this.props.time_series;
     const target_canvas = document.getElementById(this.props.id);
@@ -144,7 +69,11 @@ export default class graphContainer extends React.Component {
       <div>
         <canvas id={this.props.id} width="420" height="150" style={{left: 0, top: 0, zIndex: 0}}></canvas>
         <canvas id={this.props.id + '_highlight'} width="420" height="150" style={{position: 'absolute', display: 'block', top: 0, zIndex: 1}}></canvas>
-        <canvas id={this.props.id + '_indicator'} width="420" height="150" style={{position: 'absolute', display: 'block', top: 0, zIndex: 1}}></canvas>
+        <IndicatorCanvas
+          id={this.props.id}
+          tiff_list={this.props.tiff_list}
+          tiff_index={this.props.tiff_index}
+        />
         
         {(() => {
           if(this.props.tiff_list.length !== 0 && this.already_drawn == false) {
---
+++ front/src/components/graph/indicator-canvas.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+
+import Action from '../../actions/Actions'
+
+export default class indicatorCanvas extends React.Component {
+  constructor(props) {
+    super(props);
+    this.mouse_down = false;
+
+    this.mouseDown = this.mouseDown.bind(this);
+    this.mouseMove = this.mouseMove.bind(this);
+    this.mouseUp = this.mouseUp.bind(this);
+    this.mouseOut = this.mouseOut.bind(this);
+  }
+
+  componentDidMount() {
+    // draw a vertical line to the graph
+    this.canvas = document.getElementById(this.props.id + '_indicator');
+    this.ctx = this.canvas.getContext('2d');
+
+    this.drawVerticalLine(0, 0, 0, this.canvas.height);
+
+    this.canvas.addEventListener('mousedown', this.mouseDown)
+    this.canvas.addEventListener('mousemove', this.mouseMove)
+    this.canvas.addEventListener('mouseup', this.mouseUp)
+    this.canvas.addEventListener('mouseout', this.mouseOut);
+  }
+
+  componentDidUpdate(prevProps) {
+    // draw an indicator to show a timestep
+    if(prevProps.tiff_index !== this.props.tiff_index) {
+      const pos1 = {
+        x: this.canvas.width / this.props.tiff_list.length * this.props.tiff_index,
+        y: 0
+      };
+      this.drawVerticalLine(pos1.x, pos1.y, pos1.x, this.canvas.height);
+    }
+  }
+
+  drawVerticalLine(x1, y1, x2, y2) {
+    this.ctx.strokeStyle = 'black';
+    this.ctx.lineWidth = 2;
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    // draw lines
+    this.ctx.beginPath()
+    this.ctx.moveTo(x1, y1);
+    this.ctx.lineTo(x2, y2);
+    this.ctx.stroke();
+  }
+
+  mouseDown(e) {
+    this.updateIndexByMouse(e);
+    this.mouse_down = true;
+  }
+
+  mouseMove(e) {
+    if(this.mouse_down === false) {
+      return;
+    }
+    this.updateIndexByMouse(e);
+  }
+
+  updateIndexByMouse(e) {
+    const rect = e.target.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+
+    let new_index = Math.floor(x / this.canvas.width * this.props.tiff_list.length);
+    new_index = (new_index > 0)? new_index : 0;
+    new_index = (new_index < this.props.tiff_list.length)? new_index : this.props.tiff_list.length;
+    Action.handleIndicatorMove(new_index);
+  }
+
+  mouseUp(e) {
+    this.mouse_down = false;
+  }
+
+  mouseOut(e) {
+    if(this.mouse_down === true) {
+      this.mouse_down = false;
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <canvas id={this.props.id + '_indicator'} width="420" height="150" style={{position: 'absolute', display: 'block', top: 0, zIndex: 2}}></canvas>
+      </div>
+    );
+  }
+}
