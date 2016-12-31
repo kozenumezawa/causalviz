7741bac2c43272ad1d0747562427178261587b80
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-12-31 17:09:19 +0900

    Add: to calculate k-means by using .CSV files

--- python/clustering.py
+++ python/clustering.py
@@ -2,13 +2,22 @@
 import numpy as np
 import csv
 
+import k_means
+import hierarchical_clustering
+
 all_time_series = []
 for i in range(15):
     file_name = './timeseries_data/timeseries_' + str(i) + '.csv'
     f = open(file_name, 'r')
     dataReader = csv.reader(f)
     for row in dataReader:
-        all_time_series.append(row)
-test = np.array(all_time_series)
+        all_time_series.append(row[0:80])
 
-print(test.shape)  #
+# print(test.shape)  #-> (37050, 80)
+
+n_clusters = 6
+test = k_means.clustering(all_time_series, n_clusters)
+
+hierarchical_clustering.clustering(all_time_series)
+
+print(test)
--- python/hierarchical_clustering.py
+++ python/hierarchical_clustering.py
@@ -1,14 +1,8 @@
-# coding: utf-8
-import numpy as np
-import csv
+import scipy.spatial.distance
 
-all_time_series = []
-for i in range(15):
-    file_name = './timeseries_data/timeseries_' + str(i) + '.csv'
-    f = open(file_name, 'r')
-    dataReader = csv.reader(f)
-    for row in dataReader:
-        all_time_series.append(row)
-test = np.array(all_time_series)
+def clustering(all_time_series):
+    from  scipy.spatial.distance  import pdist
+    from scipy.cluster.hierarchy import linkage, dendrogram
+    from matplotlib.pyplot import show
 
-print(test.shape)  #
+    pdist = pdist(all_time_series)
