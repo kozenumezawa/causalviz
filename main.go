7447d4f0db97a0e73a24075be9b194925990440d
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2017-02-02 22:12:18 +0900

    Add: add a file to write a server side

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func handler(c *gin.Context) {
+	c.String(200, "AMAZON")
+}
+
+func main() {
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+
+	r.GET("/", handler)
+	r.Run("127.0.0.1:8000")
+}
