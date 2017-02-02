0a6e076e31dcc33b0f88b5ff5a8449e5f5e7f2ec
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2017-02-02 22:46:06 +0900

    Update: to host index.html

--- main.go
+++ main.go
@@ -5,14 +5,11 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func handler(c *gin.Context) {
-	c.String(200, "AMAZON")
-}
-
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 
-	r.GET("/", handler)
+	r.StaticFile("/", "front/dist/index.html")
+	r.Static("/front", "front")
 	r.Run("127.0.0.1:3000")
 }
