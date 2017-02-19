b1c8d747e34f2ffb4e1df09d0b83707f1043f665
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2017-02-19 15:23:27 +0900

    Refactor:

--- front/src/utils/drawing-tool.js
+++ front/src/utils/drawing-tool.js
@@ -69,8 +69,8 @@ export function hslToRgb(h, s, l) {
       return p;
     };
 
-    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-    var p = 2 * l - q;
+    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    const p = 2 * l - q;
     r = hue2rgb(p, q, h + 1/3);
     g = hue2rgb(p, q, h);
     b = hue2rgb(p, q, h - 1/3);
