24c4d6d50d086fcf532c22c76f232a6b66e0f1be
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2017-04-06 17:41:52 +0900

    Add: to test THREE.js

--- front/src/components/mainview/three-dim-view.jsx
+++ front/src/components/mainview/three-dim-view.jsx
@@ -6,36 +6,42 @@ const OrbitControls = require('three-orbit-controls')(THREE);
 export default class ThreeDimView extends React.Component {
   constructor(props) {
     super(props);
+  }
 
+  componentDidMount() {
     const width = 500;
     const height = 500;
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setSize(width, height);
     this.scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000);
+    this.camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000);
+    this.geometry = new THREE.BoxGeometry(20, 20, 20);
+    this.material = new THREE.MeshPhongMaterial({ color: 0x0000ff });
+    this.box = new THREE.Mesh(this.geometry, this.material);
+    this.box.position.z = 0;
 
-    const geometry = new THREE.BoxGeometry(20, 20, 20);
-    const material = new THREE.MeshPhongMaterial({ color: 0x0000ff });
-    const box = new THREE.Mesh(geometry, material);
-    box.position.z = 0;
+    this.scene.add(this.box);
+    this.light = new THREE.DirectionalLight(0xffffff);
+    this.scene.add(this.light);
+    this.light.position.set(30, 30, 30);
 
-    this.scene.add(box);
-    let light = new THREE.DirectionalLight(0xffffff);
-    this.scene.add(light);
-    light.position.set(1, 1, 1);
+    this.renderer.render(this.scene, this.camera);
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.target = new THREE.Vector3(30, 30, 30);
 
-    this.renderer.render(this.scene, camera);
-    this.controls = new OrbitControls(camera, this.renderer.domElement);
-    // this.controls.target = new THREE.Vector3(1, 1, 1);
-  }
-
-  componentDidMount() {
+    this.animate = this.animate.bind(this);
     document.getElementById('three-view').appendChild(this.renderer.domElement);
+    this.animate();
   }
-  
+
+  componentWillUnmount() {
+    this.renderer = null;
+  }
+
   animate() {
     requestAnimationFrame(this.animate);
     this.controls.update();
+    this.renderer.render(this.scene, this.camera);
   }
 
   render() {
