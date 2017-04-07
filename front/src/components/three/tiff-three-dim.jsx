977759df0f9679a7f30284b7d03a9744e8c77859
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2017-04-07 14:26:30 +0900

    Add: assign color to each box geometry

--- front/src/components/three/tiff-three-dim.jsx
+++ front/src/components/three/tiff-three-dim.jsx
@@ -16,18 +16,19 @@ export default class TiffThreeDim extends React.Component{
 
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000);
-    this.camera.position.z = 30;
+    this.camera.position.x = 100;
+    this.camera.position.y = -70;
+    this.camera.position.z = 450;
 
     this.renderer.render(this.scene, this.camera);
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-    this.controls.target = new THREE.Vector3(0, 0, 0);
+
+    this.controls.target = new THREE.Vector3(this.camera.position.x, this.camera.position.y, 0);
 
     this.animate = this.animate.bind(this);
 
     this.axis = new THREE.AxisHelper(1000);
     this.scene.add(this.axis);
-
-    this.createBox();
   }
 
 
@@ -40,7 +41,7 @@ export default class TiffThreeDim extends React.Component{
   }
 
   componentWillReceiveProps(nextProps) {
-    this.createBox(nextProps.canvas_width, nextProps.canvas_height);
+    this.createBox(nextProps.canvas_width, nextProps.canvas_height, nextProps.tiff_list, nextProps.tiff_index, nextProps.time_series);
   }
 
   animate() {
@@ -49,17 +50,28 @@ export default class TiffThreeDim extends React.Component{
     this.renderer.render(this.scene, this.camera);
   }
 
-  createBox(width, height) {
+  createBox(width, height, tiff_list, tiff_index, time_series) {
+    const canvas = tiff_list[tiff_index];
+    const ctx = canvas.getContext('2d');
+    const tiff_image = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const tiff_rgba = tiff_image.data; // image_rgba = [R, G, B, A, R, G, B, A, ...] (hex data)
+
     for (let i = 0; i < height; i++) {
       this.boxes[i] = [];
 
       for (let j = 0; j < width; j++) {
-        const length = i;
-        const geometry = new THREE.BoxGeometry(1, 1, 1);
-        const c = 0x0000ff * (i / height);
-        const material = new THREE.MeshBasicMaterial({ color: c });
-        this.boxes[i][j] = new THREE.Mesh(geometry, material);
+        const r = tiff_rgba[(i * width + j ) * 4 + 0] / 255;
+        const g = tiff_rgba[(i * width + j ) * 4 + 1] / 255;
+        const b = tiff_rgba[(i * width + j ) * 4 + 2] / 255;
+        if (r === 0 && g === 0 && b === 0) {
+          continue;
+        }
+        const pixel_color = new THREE.Color(r, g, b);
 
+        const length = time_series[i * width + j][tiff_index];
+        const geometry = new THREE.BoxGeometry(1, 1, length);
+        const material = new THREE.MeshBasicMaterial({ color: pixel_color });
+        this.boxes[i][j] = new THREE.Mesh(geometry, material);
         this.boxes[i][j].position.x = j;
         this.boxes[i][j].position.y = -i;
         this.boxes[i][j].position.z = length / 2;
