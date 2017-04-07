e12ce7d2b4c0f3aafafb2f57fed93c56ecc17b49
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2017-04-07 14:25:55 +0900

    Add: add a graph container to 3D view

--- front/src/components/mainview/three-dim-view.jsx
+++ front/src/components/mainview/three-dim-view.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import GraphContainer from '../graph/graph-container.jsx'
 import TiffThreeDim from '../three/tiff-three-dim.jsx'
 
 export default class ThreeDimView extends React.Component {
@@ -15,7 +16,19 @@ export default class ThreeDimView extends React.Component {
           canvas_height={this.props.parent_state.canvas_height}
           tiff_list={this.props.tiff_list}
           tiff_index={this.props.parent_state.tiff_index}
+          time_series={this.props.parent_state.all_time_series}
         />
+
+        <div style={{position: 'absolute', display: 'inline-block', top: 200, left: 400}}>
+          <GraphContainer
+            id="time_series_graph_1"
+            highlighted_line={this.props.parent_state.highlighted_line}
+            line_color="green"
+            tiff_index={this.props.parent_state.tiff_index}
+            tiff_list={this.props.tiff_list}
+            time_series={this.props.parent_state.all_time_series}
+          />
+        </div>
       </div>
     );
   }
