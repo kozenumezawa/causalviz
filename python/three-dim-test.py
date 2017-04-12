507e6a1c13ab9bec1b36ec7daa921a38b2cd5247
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2017-04-12 15:26:05 +0900

    Update: to show 3D

--- python/three-dim-test.py
+++ python/three-dim-test.py
@@ -1,14 +1,28 @@
 # coding: utf-8
+import csv
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.arange(0, 128, 1)
-y = np.arange(0, 96, 1)
+all_time_series = []
+for i in range(15):
+    file_name = './timeseries_data/timeseries_' + str(i) + '.csv'
+    f = open(file_name, 'r')
+    dataReader = csv.reader(f)
+    for row in dataReader:
+        all_time_series.append(row[0:80])
+all_time_series = np.array(all_time_series)
+
+x = np.arange(0, 285, 1)
+y = np.arange(0, 130, 1)
 
 X, Y = np.meshgrid(x, y)
-
-Z = np.sin(X) + np.cos(Y)
+Z = []
+for i in range(130):
+    Z.append([])
+    for j in range(285):
+        Z[i].append(float(all_time_series[285 * i + j][0]))
+Z = np.array(Z)
 
 fig = plt.figure()
 ax = Axes3D(fig)
