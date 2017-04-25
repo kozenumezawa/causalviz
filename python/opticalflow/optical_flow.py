9b8d57cbc24f359b0a2816b3261a2389c3a9f144
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2017-04-25 10:57:22 +0900

    Add: [WIP] add unit vector according to the direction

--- python/opticalflow/optical_flow.py
+++ python/opticalflow/optical_flow.py
@@ -4,8 +4,8 @@ import csv
 import math
 
 
-def get_lag_maximizing_corr(frame, target_pixel, all_time_series, win_frames, max_lag, width, height):
-    if np.sum(all_time_series[target_pixel]) == 0:
+def get_lag_maximizing_corr(frame, center_pixel, all_time_series, win_frames, max_lag, width, height):
+    if np.sum(all_time_series[center_pixel]) == 0:
         return 0
     n_frames = all_time_series.shape[1]
     n_pixels = width * height
@@ -19,13 +19,14 @@ def get_lag_maximizing_corr(frame, target_pixel, all_time_series, win_frames, ma
         start_frame = n_frames - win_frames
 
     corr_list = []
-    pixel_list = []
+    vec_list = []
     lag_list = []
+    ROOT_INV = 1 / math.sqrt(2)
     for x in [-1, 0, 1]:
         for y in [-1, 0, 1]:
             if x == 0 and y == 0:
                 continue
-            pixel = target_pixel + x + y * width
+            pixel = center_pixel + x + y * width
             if pixel < 0 or pixel >= n_pixels:
                 continue
             y_time_series = all_time_series[pixel][start_frame:stop_frame]
@@ -37,19 +38,36 @@ def get_lag_maximizing_corr(frame, target_pixel, all_time_series, win_frames, ma
             for lag in range(0, lag_range):
                 if start_frame + lag < 0 or stop_frame + lag >= n_frames:
                     continue
-                target_time_series = all_time_series[target_pixel][start_frame + lag : stop_frame + lag]
+                target_time_series = all_time_series[center_pixel][start_frame + lag : stop_frame + lag]
                 corr_list.append(np.corrcoef(target_time_series, y_time_series)[0][1])
-                pixel_list.append(pixel)
                 lag_list.append(lag)
+                if [x, y] == [-1, -1]:
+                    vec_list.append(np.array([-ROOT_INV, -ROOT_INV]))
+                elif [x, y] == [-1, 0]:
+                    vec_list.append(np.array([-1, 0]))
+                elif [x, y] == [-1, 1]:
+                    vec_list.append(np.array([-ROOT_INV, ROOT_INV]))
+                elif [x, y] == [0, -1]:
+                    vec_list.append(np.array([0, -1]))
+                elif [x, y] == [0, 1]:
+                    vec_list.append(np.array([0, 1]))
+                elif [x, y] == [1, -1]:
+                    vec_list.append(np.array([ROOT_INV, -ROOT_INV]))
+                elif [x, y] == [1, 0]:
+                    vec_list.append(np.array([1, 0]))
+                elif [x, y] == [1, 1]:
+                    vec_list.append(np.array([ROOT_INV, ROOT_INV]))
 
     max_corr = np.max(corr_list)
     max_index = np.argmax(np.array(corr_list))
-    maximum_pixel = pixel_list[max_index]
-    maximum_lag = lag_list[max_index]
+    max_lag = lag_list[max_index]
+    vector = np.array(vec_list[max_index])
 
-    if maximum_lag > 0:
-        print(frame, max_corr, maximum_pixel, maximum_lag)
-    return 1
+    if max_lag <= 0:
+        vector = np.array([0, 0])
+
+    # Todo: Calculate vector    
+    print max_lag, vector
 
 if __name__ == "__main__":
     all_time_series = []
@@ -71,7 +89,8 @@ if __name__ == "__main__":
     width = 285
     height = 130
     for frame in range(all_time_series.shape[1]):
-        for (target_pixel, time_series) in enumerate(all_time_series):
-            lag = get_lag_maximizing_corr(frame, target_pixel, all_time_series, win_frames, max_lag, width, height)
+        print frame
+        for (center_pixel, time_series) in enumerate(all_time_series):
+            lag = get_lag_maximizing_corr(frame, center_pixel, all_time_series, win_frames, max_lag, width, height)
             if lag == 0:
                 lag = 'error'
