1abdafdf8f3a6ebd3e0dceb32e56e07bf47f81a9
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2017-04-25 11:29:46 +0900

    Add: Move file and add lic

--- python/lic/optical_flow.py
+++ python/lic/optical_flow.py
@@ -19,8 +19,9 @@ def get_lag_maximizing_corr(frame, center_pixel, all_time_series, win_frames, ma
         start_frame = n_frames - win_frames
 
     corr_list = []
-    vec_list = []
     lag_list = []
+    pixel_list = []
+    vec_list = []
     ROOT_INV = 1 / math.sqrt(2)
     for x in [-1, 0, 1]:
         for y in [-1, 0, 1]:
@@ -38,9 +39,10 @@ def get_lag_maximizing_corr(frame, center_pixel, all_time_series, win_frames, ma
             for lag in range(0, lag_range):
                 if start_frame + lag < 0 or stop_frame + lag >= n_frames:
                     continue
-                target_time_series = all_time_series[center_pixel][start_frame + lag : stop_frame + lag]
-                corr_list.append(np.corrcoef(target_time_series, y_time_series)[0][1])
+                center_time_series = all_time_series[center_pixel][start_frame + lag : stop_frame + lag]
+                corr_list.append(np.corrcoef(center_time_series, y_time_series)[0][1])
                 lag_list.append(lag)
+                pixel_list.append(pixel)
                 if [x, y] == [-1, -1]:
                     vec_list.append(np.array([-ROOT_INV, -ROOT_INV]))
                 elif [x, y] == [-1, 0]:
@@ -61,13 +63,18 @@ def get_lag_maximizing_corr(frame, center_pixel, all_time_series, win_frames, ma
     max_corr = np.max(corr_list)
     max_index = np.argmax(np.array(corr_list))
     max_lag = lag_list[max_index]
+    max_pixel = pixel_list[max_index]
     vector = np.array(vec_list[max_index])
 
     if max_lag <= 0:
-        vector = np.array([0, 0])
+        return np.array([0, 0])
 
-    # Todo: Calculate vector    
-    print max_lag, vector
+    # Calculate the magnitude of vector
+    target_value = all_time_series[max_pixel][frame + max_lag]
+    center_value = all_time_series[center_pixel][frame]
+    vector = vector * (target_value - center_value) / max_lag
+    return vector
+
 
 if __name__ == "__main__":
     all_time_series = []
@@ -88,9 +95,25 @@ if __name__ == "__main__":
     max_lag = 10
     width = 285
     height = 130
-    for frame in range(all_time_series.shape[1]):
-        print frame
+    vectors = np.zeros((height, width,2),dtype=np.float32)
+    # for frame in range(all_time_series.shape[1]):
+    for frame in range(1):
         for (center_pixel, time_series) in enumerate(all_time_series):
-            lag = get_lag_maximizing_corr(frame, center_pixel, all_time_series, win_frames, max_lag, width, height)
-            if lag == 0:
-                lag = 'error'
+            vector = get_lag_maximizing_corr(frame, center_pixel, all_time_series, win_frames, max_lag, width, height)
+            vectors[center_pixel / width][center_pixel % width] = vector
+
+    texture = np.random.rand(size,size).astype(np.float32)
+
+    plt.bone()
+
+    kernellen=31
+    kernel = np.sin(np.arange(kernellen)*np.pi/kernellen)
+    kernel = kernel.astype(np.float32)
+
+    image = lic_internal.line_integral_convolution(vectors, texture, kernel)
+
+    plt.clf()
+    plt.axis('off')
+    plt.figimage(image)
+    plt.gcf().set_size_inches((size/float(dpi),size/float(dpi)))
+    plt.savefig("flow-image.png",dpi=dpi)
