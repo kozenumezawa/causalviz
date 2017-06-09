2e4db030859cfe62a7a2dce9abf8912a64415f04
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2017-06-09 16:26:41 +0900

    Update: test drawing

--- front/src/components/mainview/graph-view.jsx
+++ front/src/components/mainview/graph-view.jsx
@@ -11,13 +11,21 @@ export default class GraphView extends React.Component {
     this.canvas = document.getElementById("graph");
     this.ctx = this.canvas.getContext('2d');
 
-    window.fetch('http://localhost:3000/api/v1/getcorr', {
-      mode: 'cors',
-      method: 'GET',
-      headers: {
-        'content-type': 'application/json',
-      }
-    })
+    // window.fetch('http://localhost:3000/api/v1/getcorr', {
+    //   mode: 'cors',
+    //   method: 'GET',
+    //   headers: {
+    //     'content-type': 'application/json',
+    //   }
+    // })
+    //   .then((response) => {
+    //     return response.json();
+    //   })
+    //   .then((json) => {
+    //     this.corr_list = json.data
+    //     this.drawData(this.props);
+    //   });
+    window.fetch("trp3_corr.json")
       .then((response) => {
         return response.json();
       })
@@ -25,19 +33,10 @@ export default class GraphView extends React.Component {
         this.corr_list = json.data
         this.drawData(this.props);
       });
-    // window.fetch("trp3_corr.json")
-    //   .then((response) => {
-    //     return response.json();
-    //   })
-    //   .then((json) => {
-    //     console.log(json);
-    //
-    //     this.drawData(this.props);
-    //   });
   }
 
   componentWillReceiveProps(nextProps) {
-    // this.drawData(nextProps);
+    this.drawData(nextProps);
   }
 
   drawData(props) {
@@ -50,31 +49,31 @@ export default class GraphView extends React.Component {
     if (this.corr_list == null) {
       return null
     }
+
     this.ctx.beginPath();
     this.ctx.lineWidth = 0.5;
 
     props.parent_state.all_time_series.forEach((x, x_idx) => {
-      if (x[0] !== 0) {
-        props.parent_state.all_time_series.forEach((y, y_idx) => {
-          if (y[0] !== 0) {
-            console.log(x_idx, y_idx);
-            const corr = this.corr_list[x_idx][y_idx];
-            if (corr > 0.8) {
-              this.ctx.moveTo(x_idx % this.props.canvas_width * 10, x_idx / this.props.canvas_width * 10);
-              this.ctx.lineTo(y_idx % this.props.canvas_width * 10, y_idx / this.props.canvas_width * 10);
-              this.ctx.stroke();
+      if (x_idx < 3000) {
+        if (x[0] !== 0 && x_idx % 10 === 0) {
+          props.parent_state.all_time_series.forEach((y, y_idx) => {
+            if (y[0] !== 0 && y_idx % 10 === 0) {
+              const corr = this.corr_list[x_idx][y_idx];
+              if (corr > 0.8) {
+                this.ctx.moveTo(x_idx % props.parent_state.canvas_width * 5, Math.floor(x_idx / props.parent_state.canvas_width * 5));
+                this.ctx.lineTo(y_idx % props.parent_state.canvas_width * 5, Math.floor(y_idx / props.parent_state.canvas_width * 5));
+                this.ctx.stroke();
+              }
             }
-          }
-        });
+          });
+        }
       }
     });
-
-
   }
 
   render() {
-    const width = this.props.parent_state.canvas_width * 10;
-    const height = this.props.parent_state.canvas_height * 10;
+    const width = this.props.parent_state.canvas_width * 5;
+    const height = this.props.parent_state.canvas_height * 5;
     return (
       <canvas id="graph" width={width} height={height}></canvas>
     );
