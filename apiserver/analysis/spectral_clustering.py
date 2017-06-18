feed81ab9fd487bc45f764d5edadb21abffe60b3
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2017-06-18 19:44:34 +0900

    Update: to transform matrix

--- apiserver/analysis/spectral_clustering.py
+++ apiserver/analysis/spectral_clustering.py
@@ -11,8 +11,8 @@ def draw_heatmap(data, row_labels, column_labels):
     ax.invert_yaxis()
     ax.xaxis.tick_top()
 
-    # ax.set_xticklabels(row_labels, minor=False)
-    # ax.set_yticklabels(column_labels, minor=False)
+    ax.set_xticklabels(row_labels, minor=False)
+    ax.set_yticklabels(column_labels, minor=False)
     plt.show()
     return heatmap
 
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     import csv
     from sklearn.cluster import spectral_clustering
 
-    data_step = 5
+    data_step = 4
 
     f = open("./trp3_corr.json", "r")
     json_data = json.load(f)
@@ -49,15 +49,17 @@ if __name__ == "__main__":
             if len(corr_list[pixel]) == 0:
                 continue
 
-            if corr == -2 or corr < 0.7:
+            if corr == -2 or corr < 0.8:
                 graph_row.append(0)
+            elif i == pixel:
+                graph_row.append(0)  # i == j のとき0
             else:
                 graph_row.append(corr)
         graph.append(graph_row)
         data_pixel_list.append(i)
     graph = np.array(graph)
 
-    n_clusters = 4
+    n_clusters = 5
     labels = spectral_clustering(graph, n_clusters=n_clusters)
 
     f = open('./data/graph.csv', 'w')
@@ -77,10 +79,40 @@ if __name__ == "__main__":
     f.close()
 
     # labesに沿って、グラフをソート
-    f = open('./data/graph_sorted.csv', 'w')
-    writer = csv.writer(f)
+    # 行をsort
+    graph_sorted = []
+    original_idx = []
     for i in range(n_clusters):
         for (row_idx, cluster_idx) in enumerate(labels):
             if i == cluster_idx:
-                writer.writerows([graph[row_idx]])
+                graph_sorted.append(graph[row_idx])
+                original_idx.append(row_idx)
+
+    # 列をsort
+    graph_sorted = np.array(graph_sorted)
+    graph_sorted_T = graph_sorted.transpose()
+    graph_sorted = []
+    for i in range(n_clusters):
+        for (row_idx, cluster_idx) in enumerate(labels):
+            if i == cluster_idx:
+                graph_sorted.append(graph_sorted_T[row_idx])
+    graph_sorted = np.array(graph_sorted)
+    graph_sorted = graph_sorted.transpose()
+
+    # sort後のgraphを書き込み
+    f = open('./data/graph_sorted.csv', 'w')
+    writer = csv.writer(f)
+    for row in graph_sorted:
+        writer.writerows([row])
     f.close()
+
+    # d3.jsのために書き込み
+    f = open('./data/graph_sorted_d3.csv', 'w')
+    writer = csv.writer(f)
+    writer.writerows([["original_idx", "in", "out", "value"]])
+    for (i, row) in enumerate(graph_sorted):
+        for (j, value) in enumerate(row):
+            writer.writerows([[original_idx[i], i, j, value]])
+    f.close()
+
+    draw_heatmap(graph_sorted, range(len(graph_sorted)), range(len(graph_sorted)))
