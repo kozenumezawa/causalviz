a8593f431535a60e29325660199168d0e4e24262
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2017-07-07 13:07:08 +0900

    Add: to normalize the vector

--- apiserver/analysis/ar_model_test.py
+++ apiserver/analysis/ar_model_test.py
@@ -11,8 +11,9 @@ def create_ar_modex(data):
     min_aic_lag = aic_list.index(min(aic_list)) + 1
 
     data_fit = model.fit(min_aic_lag)
+    # print data_fit.params
 
-    fit = []
+    fit = [80] * min_aic_lag
     for t in range(min_aic_lag, len(data)):
         value = data_fit.params[0]
         for i in range(2, min_aic_lag + 2):
@@ -20,12 +21,28 @@ def create_ar_modex(data):
         fit.append(value)
     return [fit, data_fit.resid]
 
+#
+def normalization(x):
+    """ normalized x into mean 0 and variance 1, max(x) <= 1"""
+    max_x = np.max(x)
+    x = x / max_x
+    x = x - np.mean(x)  # mean of x becomes 0
+    x = x / np.std(x)   # variance of x becomes 1
+    return x
+
+def getDiffTimeSeries(x, diff):
+    x_diff = []
+    for i in range(len(x) - diff):
+        x_diff.append(x[i + diff] - x[i])
+    return np.array(x_diff)
+
 if __name__ == "__main__":
     import numpy as np
     import matplotlib.pyplot as plt
     from scikits.statsmodels.tsa import stattools
     from scikits.statsmodels.tsa import ar_model
-
+    from scikits.statsmodels.tsa.stattools import grangercausalitytests as granger
+    from scikits.statsmodels.tsa.stattools import adfuller as adfuller
     mean_step = 3
 
     input_file_name = "trp3data_mean.npy"
@@ -43,30 +60,65 @@ if __name__ == "__main__":
 
 
     all_time_series = np.load("./data/" + input_file_name)
-    all_time_series = np.array(all_time_series)
+    all_time_series = np.array(all_time_series, dtype=np.float)
 
     x = all_time_series[3000]
-    y = all_time_series[8000]
+    y = all_time_series[7000]
 
-    x_fit, res = create_ar_modex(x)
+    x = normalization(x)
+    y = normalization(y)
 
+    # create difference time series to create stationary time series
+    x_diff_1 = getDiffTimeSeries(x, 1)
+    y_diff_1 = getDiffTimeSeries(y, 1)
+
+    # visualize original data
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
     ax.plot(x)
-    ax.plot(x_fit)
-    plt.legend(labels=['X', 'AR model'])
+    ax.plot(y)
+    plt.legend(labels=['X', 'Y'])
     plt.show()
-    plt.bar(range(len(res)), res)
-    plt.show()
-    acf = stattools.acf(res, nlags=100)
-    plt.bar(range(len(acf)), acf)
 
-    y_fit, res = create_ar_modex(y)
+    # visualize original data
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
-    ax.plot(y)
-    ax.plot(y_fit)
-    plt.legend(labels=['Y', 'AR model'])
+    ax.plot(x_diff_1)
+    ax.plot(y_diff_1)
+    plt.legend(labels=['X_diff_1', 'Y_diff_1'])
     plt.show()
-    plt.bar(range(len(res)), res)
-    plt.show()
+    # print adfuller(x_diff_1, regression="c")   The Augmented Dickey-Fuller test
+
+    # x_fit, res = create_ar_modex(x)
+
+    # fig = plt.figure()
+    # ax = fig.add_subplot(1,1,1)
+    # ax.plot(x)
+    # ax.plot(x_fit)
+    # plt.legend(labels=['X', 'AR model'])
+    # plt.show()
+    # plt.bar(range(len(res)), res)
+    # plt.show()
+    # acf = stattools.acf(res, nlags=100)
+    # plt.bar(range(len(acf)), acf)
+
+    # y_fit, res = create_ar_modex(y)
+    # fig = plt.figure()
+    # ax = fig.add_subplot(1,1,1)
+    # ax.plot(y)
+    # ax.plot(y_fit)
+    # plt.legend(labels=['Y', 'AR model'])
+    # plt.show()
+    # plt.bar(range(len(res)), res)
+    # plt.show()
+
+    input_list = []
+    for a, b in zip(x_diff_1, y_diff_1):
+        input_list.append([a, b])
+    results = granger(np.array(input_list), maxlag=2)
+
+    input_list = []
+    for a, b in zip(x_diff_1, y_diff_1):
+        input_list.append([b, a])
+    results = granger(np.array(input_list), maxlag=2)
+
