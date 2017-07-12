935ae8a07027b426339ede943e9c7616b896c7cc
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2017-07-12 17:15:49 +0900

    Update: delete node link visualization

--- front/src/components/mainview/graph-view.jsx
+++ front/src/components/mainview/graph-view.jsx
@@ -31,17 +31,7 @@ export default class GraphView extends React.Component {
       });
 
       // this.drawData(nextProps, name);
-
-      window.fetch(name + "_corr.json")
-        .then((response) => {
-          return response.json();
-        })
-        .then((json) => {
-          this.setState({
-            corr_list: json.data
-          });
-          this.drawData(nextProps, name);
-        });
+      this.drawData(nextProps, name);
     }
   }
 
@@ -67,72 +57,6 @@ export default class GraphView extends React.Component {
 
     const tiff_image = ctx.getImageData(0, 0, canvas.width, canvas.height);
     const tiff_rgba = tiff_image.data; // image_rgba = [R, G, B, A, R, G, B, A, ...] (hex data)
-    /*
-    const svg = d3.select("#graphsvg")
-      .append("svg");
-    svg.attr("width", width);
-    svg.attr("height", height);
-
-    const pixel_list = [];
-    const color_list = [];
-
-    props.parent_state.all_time_series.forEach((time_series, idx) => {
-        const x = idx % canvas.width;
-        const y = Math.floor(idx / canvas.width);
-
-        const circle = [x * this.scale, y * this.scale, 2];  // [x, y, r]
-        pixel_list.push(circle);
-
-        const r = tiff_rgba[idx * 4 + 0];
-        const g = tiff_rgba[idx * 4 + 1];
-        const b = tiff_rgba[idx * 4 + 2];
-        if (this.sum(time_series) !== 0 && this.isSamplingPoint(idx, canvas.width)) {
-          color_list.push([255, 0, 255]);
-        } else {
-          color_list.push([r, g, b]);
-        }
-      });
-
-    const circles = svg.selectAll('circle').data(pixel_list).enter().append('circle')
-      .attr("cx", (d) => { return d[0] })
-      .attr("cy", (d) => { return d[1] })
-      .attr("r", (d) => { return d[2] })
-      .style("fill", (d, i) => {
-        return d3.rgb(color_list[i][0], color_list[i][1], color_list[i][2]);
-      })
-      .on("mouseover", (selected_pixel) => {
-        svg.selectAll("line").remove();
-        
-        // d3.select(this).style("fill", "orange");
-        const x_idx = (selected_pixel[0] / this.scale + selected_pixel[1] / this.scale * canvas.width);
-        const x = props.parent_state.all_time_series[x_idx];
-        if (this.sum(x) === 0) {
-          return;
-        }
-
-        props.parent_state.all_time_series.forEach((y, y_idx) => {
-          if (this.sum(y) !== 0 && this.isSamplingPoint(y_idx, canvas.width) && this.state.corr_list[x_idx].length !== 0) {
-            const corr = this.state.corr_list[x_idx][y_idx];
-            if (corr > 0.9) {
-              svg.append("line").data(pixel_list)
-                .style("stroke", "black")  // colour the line
-                .attr("x1", (d) => {
-                  return selected_pixel[0]
-                })
-                .attr("y1", (d) => {
-                  return selected_pixel[1]
-                })
-                .attr("x2", (d) => {
-                  return pixel_list[y_idx][0]
-                })
-                .attr("y2", (d) => {
-                  return pixel_list[y_idx][1]
-                });
-            }
-          }
-        });
-      });
-     */
     
     // draw heatmap and canvas according to the graph
     window.fetch(name + '_graph_sorted.json')
