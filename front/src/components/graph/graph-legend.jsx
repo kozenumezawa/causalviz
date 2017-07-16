c22be15c489026d6382e597f4166d6a21b99c9c9
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2017-07-16 23:23:36 +0900

    Update: update graph label position

--- front/src/components/graph/graph-legend.jsx
+++ front/src/components/graph/graph-legend.jsx
@@ -11,9 +11,9 @@ export default class GraphLegend extends React.Component {
         <div style={{position: 'relative', display: 'inline-block', top:-160, left: -45}}> 100 </div>
         <div style={{position: 'relative', display: 'inline-block', top:-20, left: -70}}> -50 </div>
 
-        <div style={{position: 'relative', display: 'inline-block', top:0, left: -70}}> -1.8 </div>
+        <div style={{position: 'relative', display: 'inline-block', top:0, left: -60}}> -1.8 </div>
         <div style={{position: 'relative', display: 'inline-block', top:0, left: 120}}> Time (s)</div>
-        <div style={{position: 'relative', display: 'inline-block', top:0, left: 260}}> 32</div>
+        <div style={{position: 'relative', display: 'inline-block', top:0, left: 290}}> 32</div>
       </div>
     );
   }
