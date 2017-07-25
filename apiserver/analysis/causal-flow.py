e4ffa4e9c20115f5048b490867a75a16b3cce939
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2017-07-25 22:17:16 +0900

    Update: to realize block-based Causal Flow

--- apiserver/analysis/causal-flow.py
+++ apiserver/analysis/causal-flow.py
@@ -21,6 +21,8 @@ def calcCausalFlow(granger_list):
 
 if __name__ == "__main__":
     mean_step = 3
+    block_r = 1     # radius of each block
+    block_idx = [i for i in range(-block_r, block_r + 1)]
 
     # input_file_name = "trp3data_mean.npy"
     # output_file_name = "trp3_flow.json"
@@ -37,30 +39,41 @@ if __name__ == "__main__":
 
 
     all_time_series = np.load("./data/" + input_file_name)
-
+    all_time_series = all_time_series.astype(np.float32)
     vector_list = []
 
     for (i, _) in enumerate(all_time_series):
-        x = all_time_series[i]
-        x = x.astype(np.float32)
-        if (sum(x) == 0 or (i % width) % mean_step != 1 or math.floor(i / width) % mean_step != 0):
+        if (sum(all_time_series[i]) == 0 or (i % width) % mean_step != 1 or math.floor(i / width) % mean_step != 0):
             vector_list.append([])
             continue
+        x_block = []
+        for y_idx in block_idx:
+            for x_idx in block_idx:
+                x_block.append(all_time_series[i + x_idx + (width * y_idx)])
+        x_block = np.array(x_block)
+        x_block = x_block.T
 
         granger_list = []
-        for x_idx in [-mean_step, 0, mean_step]:
+        for block_x_idx in [-mean_step, 0, mean_step]:
             row_granger = []
-            for y_idx in [-mean_step, 0, mean_step]:
-                if x_idx == 0 and y_idx == 0:
+            for block_y_idx in [-mean_step, 0, mean_step]:
+                if block_x_idx == 0 and block_y_idx == 0:
                     row_granger.append(0)
                     continue
-                y = all_time_series[i + x_idx + y_idx * width]
-                y = y.astype(np.float32)
-                if (sum(y) == 0):
+                y_block_center_idx = i + block_x_idx + block_y_idx * width
+                if (sum(all_time_series[y_block_center_idx]) == 0):
                     row_granger.append(0)
                     continue
-                calc_yx = CausalCalculator(X=y[:, np.newaxis], Y_cause=x[:, np.newaxis])
-                k, m = 1, 5
+
+                y_block = []
+                for y_idx in block_idx:
+                    for x_idx in block_idx:
+                        y_block.append(all_time_series[y_block_center_idx + x_idx + (width * y_idx)])
+                y_block = np.array(y_block)
+                y_block = y_block.T
+
+                calc_yx = CausalCalculator(X=y_block, Y_cause=x_block)
+                k, m = 1, 1
                 Gx_to_y = calc_yx.calcGrangerCausality(k=k, m=m)
                 row_granger.append(Gx_to_y)
             granger_list.append(row_granger)
